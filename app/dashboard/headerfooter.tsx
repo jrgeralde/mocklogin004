54af52cb8133fa05c2cf60609a8f1ca1cbd5dc13
jrgeralde <jrgeralde@gmail.com>
2026-01-11 16:04:24 +0800

    with comments

--- app/dashboard/headerfooter.tsx
+++ app/dashboard/headerfooter.tsx
@@ -1,3 +1,5 @@
+// app/dashboard/headerfooter.tsx
+
 'use client';
 
 import Link from 'next/link';
--- components/LoginPageGuard.tsx
+++ components/LoginPageGuard.tsx
@@ -1,3 +1,5 @@
+// components/LoginPageGuard.tsx
+
 'use client';
 
 import { useEffect, useState } from 'react';
